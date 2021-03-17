fdc0c61e2dad9cddef962bd721af03ed9d61ba3b
zcw <17702434673@163.com>
2021-03-17 17:35:41 +0800

    add countryDict

---
+++ constant.go
@@ -0,0 +1,11 @@
+package lazadago
+
+var CountryDict = map[string]struct{}{
+	"sg": {},
+	"th": {},
+	"my": {},
+	"vn": {},
+	"ph": {},
+	"id": {},
+	"cb": {},
+}
--- lazadago_test.go
+++ lazadago_test.go
@@ -6,6 +6,5 @@ import (
 )
 
 func TestAuthURL(t *testing.T) {
-	client := NewClient(&ClientOptions{APIKey: "120429"})
-	fmt.Println(client.GetAuthURL("120429", "https://erp.ds1689.cn/manystore/shop/shop/lazadabindget", "cn"))
+	fmt.Println(GetAuthURL("120429", "https://erp.ds1689.cn/manystore/shop/shop/lazadabindget", "cn"))
 }
